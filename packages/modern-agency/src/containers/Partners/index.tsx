cadc904661552edd5621bb2b271d74a788f07b69
OddlyTimbot <timbot@oddlystudios.com>
2020-04-04 12:54:11 -0400

    updated partnership page

--- packages/modern-agency/src/containers/Partners/index.tsx
+++ packages/modern-agency/src/containers/Partners/index.tsx
@@ -44,7 +44,7 @@ interface AboutProps {}
 const About: React.FunctionComponent<AboutProps> = props => {
   const Data = useStaticQuery(graphql`
     query {
-      avatar: file(absolutePath: { regex: "/about.jpg/" }) {
+      avatar: file(absolutePath: { regex: "/partners.jpg/" }) {
         childImageSharp {
           fluid(maxWidth: 1770, quality: 90) {
             ...GatsbyImageSharpFluid
@@ -63,12 +63,15 @@ const About: React.FunctionComponent<AboutProps> = props => {
   return (
     <AboutWrapper>
       <AboutPageTitle>
-        <h2>About TTLMakerspace Partners</h2>
+        <h2>Partnerships at TTLMakerspace</h2>
         <p>
-          The TTLMakerspace is a well-equipped community center, where makers of
-          all kinds come to work on projects, share information, participate in
-          events, and learn valuable skills.
+          Partnerships work at TTLMakerspace. Our well-equipped facilities and
+          knowledgeable community of makers provide a welcome venue for groups
+          that want to do something special, or host their own activity. Check
+          out some of our partners below.
         </p>
+        <h3>Interested in Partnership?</h3>
+        <p>Email to "partnership" at "ttlmakerspace - dot - com"</p>
       </AboutPageTitle>
 
       <AboutImage>
@@ -76,18 +79,35 @@ const About: React.FunctionComponent<AboutProps> = props => {
       </AboutImage>
 
       <AboutDetails>
-        <h2>Hey there, what’s up?</h2>
+        <h2>What kind of partnerships are available?</h2>
         <p>
-          RedQ Team is a creative agency specializing in building scalable,
-          high-performance web & mobile application. Our main concern is
-          creating more value into the application so that can help our
-          customers to grow their business.
+          The TTLMakerspace is 3000 feet of space equipped with a full Woodshop,
+          3D Printers, Laser Cave, CNC Mill Room, Computer Lab, and Workshop.
+          The venue can be booked for individual special events, or we can host
+          a course or workshop you wish to run over time.
         </p>
         <p>
           RedQ Team is a creative agency specializing in building scalable,
           high-performance web & mobile application. Our main concern is
           creating more value into the application so that can help our
           customers to grow their business.
+        </p>
+        <h2>Who are some of your partners?</h2>
+        <p>
+          StopGap is an organization whose mission is to make Toronto's building
+          easily accessible to those with special needs. As a partner to the
+          TTLMakerspace, StopGap produces their accessiblity ramps from our
+          shop, and regularly hosts paint-days for their sponsors. In addition,
+          they do instruction to raise awareness with young makers in our
+          workshop.
+        </p>
+        <p>
+          The Alzheimer's Society of Toronto runs a repeating workingshop out of
+          the TTL Makerspace, using the workshop, woodshop, and our complement
+          of tools. This program, called "Men's Shed" welcomes people diagnosed
+          in the early stages of dementia. The program is supervised by
+          TTL-trained care workers, and helps participants get past the effects
+          of isolation and depression by coming together to build projects.
         </p>
 
         <SocialProfiles>
