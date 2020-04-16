0064802437382eb73156327a8b2dab57a8bd944a
OddlyTimbot <timbot@oddlystudios.com>
2020-04-16 08:57:38 -0400

    adding google analytics

--- packages/modern-agency/gatsby-config.js
+++ packages/modern-agency/gatsby-config.js
@@ -86,7 +86,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-142542849-2`,
       },
     },
     {
